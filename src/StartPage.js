19555a1677f525480d17db595140dfadd7760454
Marcelo Monteiro <monteiromarceloc@gmail.com>
2019-02-20 12:52:23 -0300

    trying to build a Modal

--- src/StartPage.js
+++ src/StartPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './index.css';
 import MyHeader from './MyHeader.js';
+import ModalPage from './ModalPage.js';
+
 
 
 class StartPage extends Component {
@@ -14,6 +16,7 @@ class StartPage extends Component {
          <b>Achtung: </b><br></br>Nachdem Sie es einmal gefunden haben, wählen Sie es nicht mehr aus!
         </p><br></br>
         <button onClick={this.props.onStartPress} className="btn-start">Start</button>
+        <ModalPage/>
       </div>
     );
   }
